c292689714b2eae404e628d2e86f0bf65f1c53d0
MasonTW <mingxin.wang@thoughtworks.com>
2022-08-04 00:11:28 +0800

    mingxin[config]add simple controller

--- src/main/kotlin/com/mx/blog/controller/IndexController.kt
+++ src/main/kotlin/com/mx/blog/controller/IndexController.kt
@@ -1,3 +1,14 @@
+package com.mx.blog.controller
+
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
+class IndexController {
+    @GetMapping("/index")
+    @ResponseBody
+    fun index(): String{
+        return "aa"
+    }
+}
