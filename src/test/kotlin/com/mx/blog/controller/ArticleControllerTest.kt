19f99814f4513b7f8e5d35f8aa5031c4740568bf
MasonTW <mingxin.wang@thoughtworks.com>
2022-11-21 19:12:48 +0800

    mingxin[test] add tests in article controller to check no token request situation

--- src/test/kotlin/com/mx/blog/controller/ArticleControllerTest.kt
+++ src/test/kotlin/com/mx/blog/controller/ArticleControllerTest.kt
@@ -1,17 +1,26 @@
 package com.mx.blog.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.mx.blog.DTO.ArticleBasicDTO
 import com.mx.blog.DTO.ArticleInfoDTO
+import com.mx.blog.DTO.UserDTO
 import com.mx.blog.service.ArticleService
+import com.mx.blog.utils.JWTUtils
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.never
+import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import kotlin.random.Random
 
 @WebMvcTest(ArticleController::class)
 class ArticleControllerTest {
@@ -19,10 +28,14 @@ class ArticleControllerTest {
     @Autowired
     private lateinit var mockmvc: MockMvc
 
-    @MockBean
-    private lateinit var  article: ArticleService
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
     private lateinit var articleDTO: ArticleInfoDTO
-    private val mockArticleService = mock(ArticleService::class.java)
+    private lateinit var token: String
+
+    @MockBean
+    private lateinit var mockArticleService: ArticleService
 
     @BeforeEach
     fun beforeEach() {
@@ -31,29 +44,106 @@ class ArticleControllerTest {
             articleContent = "testContent",
             commentsNum = 0,
             agreementNum = 0,
+            relationship = ArticleInfoDTO.Relationship(isAgreeing = false, isAuthor = false)
+        )
+        token = JWTUtils.getToken(UserDTO(1, "mason"))
+    }
+
+    @Nested
+    inner class CallApiWithoutTokenTests {
+        private val articleId = Random.nextLong()
+
+        @Test
+        fun `should return unauthorized status when create article without token`() {
+            val request = MockMvcRequestBuilders.post("/articles")
+
+            mockmvc.perform(request)
+                .andExpect(status().isUnauthorized)
+
+            verify(mockArticleService, never()).createArticle(any(), any())
+        }
+
+        @Test
+        fun `should return unauthorized status when get article without token`() {
+            val request = MockMvcRequestBuilders.get("/articles/$articleId")
+
+            mockmvc.perform(request)
+                .andExpect(status().isUnauthorized)
+
+            verify(mockArticleService, never()).getArticlesById(any(), any())
+        }
+
+        @Test
+        fun `should return unauthorized status when delete article without token`() {
+            val request = MockMvcRequestBuilders.delete("/articles/$articleId")
+
+            mockmvc.perform(request)
+                .andExpect(status().isUnauthorized)
+
+            verify(mockArticleService, never()).deleteArticle(any())
+        }
+
+        @Test
+        fun `should return unauthorized status when get article by id without token`() {
+            val request = MockMvcRequestBuilders.get("/articles/$articleId")
+
+            mockmvc.perform(request)
+                .andExpect(status().isUnauthorized)
+
+            verify(mockArticleService, never()).getArticlesById(any(), any())
+        }
+
+        @Test
+        fun `should return unauthorized status when get article by user name without token`() {
+            val request = MockMvcRequestBuilders.get("/articles/testName")
+
+            mockmvc.perform(request)
+                .andExpect(status().isUnauthorized)
+
+            verify(mockArticleService, never()).getArticlesByUserName(any())
+        }
+
+        @Test
+        fun `should return ok status when get random articles successful`() {
+            `when`(mockArticleService.getRandomTenArticles()).thenReturn(listOf(articleDTO))
+
+            val request = MockMvcRequestBuilders.get("/articles/random-articles")
+
+            mockmvc.perform(request).andExpect(status().isOk)
+        }
+
+
+    }
+
+    @Test
+    fun `should return ok status when create article with token`() {
+        val request = MockMvcRequestBuilders.post("/articles")
+            .header("token", token)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(ArticleBasicDTO("title", "content")))
+
+        mockmvc.perform(request)
+            .andExpect(status().isOk)
+
+        verify(mockArticleService).createArticle(
+            ArticleBasicDTO("title", "content"), 1L
         )
     }
 
-    @Test
-    fun `should return articles when get random articles`(){
-       `when`(mockArticleService.getRandomTenArticles()).thenReturn(listOf(articleDTO))
 
-        val request = MockMvcRequestBuilders.get("/article/random")
-
-        mockmvc.perform(request).andExpect(status().isOk)
-
-
-    }
 
     @Test
-    fun `should return articles when get random articles2`(){
-        val request = MockMvcRequestBuilders.post("/article")
+    fun `should return ok status when update article successful`() {
+        val request = MockMvcRequestBuilders.put("/articles/1")
+            .header("token", token)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(
+                objectMapper.writeValueAsString(ArticleBasicDTO("title", "content"))
+            )
 
-       mockmvc.perform(request)
+        mockmvc.perform(request)
             .andExpect(status().isOk)
-
-
-
-
     }
- }
+}
